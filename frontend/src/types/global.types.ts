9f09d4ff9d592f29a02650a17e95c6ce793583e6
Ege Uzun <eegeuzunn@gmail.com>
2024-08-02 03:30:53 +0300

    feat: create const and type file

---
+++ frontend/src/types/global.types.ts
@@ -0,0 +1,14 @@
+export type email = {
+    authorFullName: string;
+    phoneNumber: string;
+    title: string;
+    description: string;
+}
+
+export type registerType = {
+    name: string;
+    surname:string
+    email:string;
+    password: string;
+    phoneNumber: string
+}
